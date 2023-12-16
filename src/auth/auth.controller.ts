18857b535ce6d908a803fc7bd2a7ffb49a0d23b8
asit-149 <asitsahoo622@gmail.com>
2023-12-16 18:21:04 +0530

    first commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService:AuthService){}
-   
-    @UseGuards(AuthGuard('local'))
-    @Post('/login')
-    async login(@Body() loginDto:any){
+  constructor(private authService: AuthService) {}
 
-     return this.authService.validateUser(loginDto.email,loginDto.password)
-    }
+  @UseGuards(AuthGuard('local'))
+  @Post('/login')
+  async login(@Request() req: any) {
+    return req.user;
+  }
 }
